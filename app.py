048deb7498b57b682f9c1ff49e4a393ff113953b
vance-coder <vance.coder@139.com>
2021-05-28 00:18:14 +0800

    commit project

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import json
 from flask_cors import CORS
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template, send_from_directory, make_response
 from dbHelper import DBHelper
-
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -15,23 +12,88 @@ def upload():
         if not files:
             print('It is None')
         for f in files:
-            f.save(f'static/images/upload/{f.filename}')
+            f.save(f'./static/images/upload/{f.filename}')
     return 'upload success'
-
 
 @app.route("/nextPicture")
 def next_picture():
+    args = request.args
     db = DBHelper()
-    data = db.select(limit=1)[0]
+    fetch_status = 0
+    if args:
+        # save value
+        value = args.get('value')
+        filename = args.get('filename')
+        status = args.get('status')
+        fetch_status = args.get('fetch_status')
+        db.update(filename, value, status)
+    data = db.select_by_status(fetch_status)
+    if data:
+        data = data[0]
+    else:
+        data = {}
+    db.close()
     return json.dumps(data)
 
+@app.route("/delete")
+def delete():
+    args = request.args
+    db = DBHelper()
+    if args:
+        # save value
+        filename = args.get('filename')
+        db.delete(filename)
+        data = {
+            'message': 'OK'
+        }
+    else:
+        data = {
+            'message': 'No data'
+        }
+    db.close()
+    return json.dumps(data)
+
+@app.route("/dashboard")
+def dashboard():
+    db = DBHelper()
+    data = {
+        'todo': 0,
+        'checked': 0,
+        'pending': 0
+    }
+    for row in db.select():
+        if row['status'] == 0:
+            data['todo'] += 1
+        elif row['status'] == 1:
+            data['checked'] += 1
+        elif row['status'] == 2:
+            data['pending'] += 1
+    db.close()
+    return json.dumps(data)
+
+@app.route("/download")
+def download():
+    db = DBHelper()
+    db.export()
+    db.close()
+    filename = 'label.zip'
+    response = make_response(
+        send_from_directory('static/', filename.encode('utf-8').decode('utf-8'), as_attachment=True))
+    response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode().decode('latin-1'))
+    return response
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 if __name__ == '__main__':
     # TODO upload file to backend call paddleOCR
     # TODO design table and process image then save result to table
+    #  show parent picture
+    #  adjust picture size (mouse)
+    #  p pending / 20 min
+    # ---- next & previous / 30 min
+    #  specific status (user can choice what status he want to show) / 1 hours
+    #  status color / 20 min
+    # TODO line number
     app.run(host="0.0.0.0", port=8080, debug=True)
--- dbHelper.py
+++ dbHelper.py
@@ -1,11 +1,15 @@
+import os
+import shutil
+import zipfile
 import sqlite3
+from glob import glob
 
 
 class DBHelper():
     def __init__(self):
         self.conn = sqlite3.connect('data.db')
         self.cursor = self.conn.cursor()
-        self.columns = ['filename', 'parent', 'confidence', 'value', 'corrected', 'uncertain', 'datetime']
+        self.columns = ['filename', 'parent', 'confidence', 'value', 'status', 'datetime']
         # 如果不存在表则创建表
         self.create_table()
 
@@ -16,8 +20,7 @@ class DBHelper():
             `parent` varchar(50),  
             `confidence` varchar(10) NOT NULL,
             `value` varchar(128) NOT NULL,
-            `corrected` NUMERIC default (0),
-            `uncertain` NUMERIC default (0),
+            `status` NUMERIC default (0),
             `datetime` TIMESTAMP default (datetime('now', 'localtime')),
             PRIMARY KEY (filename));
         '''
@@ -31,22 +34,75 @@ class DBHelper():
         print(f"Insert into data, {res.rowcount} rows effected!")
         return res.lastrowid
 
-    def select(self, limit=10):
-        sql = f"SELECT * FROM DATA ORDER BY filename DESC LIMIT {limit};"
+    def select(self, limit=10000000, offset=0):
+        sql = f"SELECT * FROM DATA ORDER BY filename DESC LIMIT {limit} OFFSET {offset};"
         res = self.cursor.execute(sql)
         return [dict(zip(self.columns, row)) for row in res.fetchall()]
 
     def select_by_filename(self, filename):
-        sql = f"SELECT * FROM DATA WHERE id={filename};"
+        sql = f"SELECT * FROM DATA WHERE filename='{filename}';"
         res = self.cursor.execute(sql)
         return dict(zip(self.columns, res.fetchall()[0]))
 
+    def select_by_status(self, value):
+        sql = f"SELECT * FROM DATA WHERE status={value};"
+        res = self.cursor.execute(sql)
+        return [dict(zip(self.columns, row)) for row in res.fetchall()]
+
+    def update(self, filename, value, status):
+        sql = f'UPDATE data SET value="{value}", status = {status} WHERE filename="{filename}";'
+        res = self.cursor.execute(sql)
+        self.conn.commit()
+        print(f"Insert into data, {res.rowcount} rows effected!")
+        return res.lastrowid
+
+    def update_status(self, filename, status):
+        sql = f"UPDATE data SET status = {status} WHERE filename='{filename}';"
+        res = self.cursor.execute(sql)
+        self.conn.commit()
+        print(f"Insert into data, {res.rowcount} rows effected!")
+        return res.lastrowid
+
+    def delete(self, filename):
+        sql = f"DELETE FROM data WHERE filename='{filename}';"
+        res = self.cursor.execute(sql)
+        self.conn.commit()
+        print(f"Insert into data, {res.rowcount} rows effected!")
+        return res.lastrowid
+
     def close(self):
         self.cursor.close()
+
+    def export(self):
+        image_folder = 'static/images/result/'
+        output = 'output/'
+        try:
+            shutil.rmtree(output)
+        except Exception as e:
+            print(e)
+        os.mkdir(output)
+        text = []
+        for row in self.select_by_status(1):
+            filepath = image_folder + row['filename']
+            output_path = output + row['filename']
+            row_text = f"{row['filename']}\t{row['value']}"
+            shutil.copy(filepath, output_path)
+            text.append(row_text)
+        label_file = output + 'label.txt'
+        with open(label_file, 'w+', encoding='utf-8') as fp:
+            fp.write('\n'.join(text))
+        with zipfile.ZipFile('static/label.zip', "w", zipfile.ZIP_DEFLATED) as zf:
+            for f in glob(output + '*'):
+                fs, filename = f.split('\\')
+                zf.write(f, filename)
 
 
 if __name__ == '__main__':
     db = DBHelper()
     lst = ['3801_anyone got.png', '1837878_3.png', '0.95', 'Value paste My OCR ']
     # db.insert(lst)
-    print(db.select())
+    # print(db.select(limit=1, offset=1))
+    # print(db.select_by_filename('fea4028b88809fff.png'))
+    # print(db.select_by_status(1))
+    # print(db.update_correct('ff9fd0e0e0c080df.png', 1))
+    db.export()
